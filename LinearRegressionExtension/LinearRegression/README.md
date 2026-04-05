c9c3460223f6e88f6f2e6ef83f66cb4812bdd1e4
Kraft-Single <cassandre.hamel.1@umontreal.ca>
2026-04-05 13:36:41 -0400

    Wanted to avoid breaking anything I cloned the linear regression and added linear, nonlinear and nonlinear function flags then in train I added some possible nonlinear functions Algorithm also will either be MLP or linear based on the linear flag For the dataset I also added the linear flag so that it can generate linear or nonlinear synthetic data I also added TODOs for things I didn't get around to doing yet Note: for having more complex mixes of variables we can just update the sweeps

---
+++ LinearRegressionExtension/LinearRegression/README.md
@@ -0,0 +1,60 @@
+# Linear regression experiments
+This sub-repo contains code for running experiments on the linear regression dataset of our paper:
+
+<p align="center">
+  <img src="https://github.com/cianeastwood/qrm/blob/main/assets/overview_lr.png?raw=true" width="700" alt="LR dataset" />
+</p>
+
+## Requirements
+We used Python 3.10. The requirements can then be installed using:
+```bash
+pip install -r requirements.txt
+```
+
+## Single run
+
+```bash
+python train.py --alg EQRM --dataset Example2 --n_envs 50 --n_samples 50000 --hparams_fixed alpha=0.99
+```
+
+## Reproducing results (multiple runs)
+### 1. Create sweep commands
+Create a text file of commands `job_scripts/reproduce.txt`, specifying the _absolute_ path to your output directory:
+```bash
+python job_scripts/gen_exps.py --output_dir /my/output/dir
+```
+
+### 2. Run the commands
+Run the commands in the text file. To do so on a local machine (warning: may take a while!), use:
+```bash
+source ./job_scripts/reproduce.txt
+```
+
+To do so via a slurm cluster, the script `job_scripts/submit_jobs.py` may provide a useful starting point, editing where necessary with the details of your cluster. After installing [submitit](https://github.com/facebookincubator/submitit), the following command will then run the commands/jobs in the text file:
+```bash
+python job_scripts/submit_jobs.py -c job_scripts/reproduce.txt
+```
+
+### 3. View results
+Sweep results will have been saved to results/reproduce/quantiles. View the results of this sweep using:
+
+```bash
+python collect_results.py /my/output/dir/reproduce/quantiles
+```
+
+### 4. Plot results
+To plot the results, and reproduce Figures 3a--3d of our paper, some dependencies may need to be installed to render latex text with matplotlib. We hope to remove these dependecies in the future. With Mac OS, it is usually sufficient to uncomment the `mpl.use('macOsX')` line at the top of `plotting.py`. With Ubuntu, following commands should be sufficient:
+```bash
+sudo apt install cm-super 
+sudo apt install dvipng
+```
+
+With these dependencies installed, the results can be plotted using the following command, saving figures to `figs/` by default:
+```bash
+python collect_results.py /my/output/dir/reproduce/quantiles --plot
+```
+
+## Acknowledgements
+
+This repo structure was based on that of
+[Linear unit-tests for invariance discovery](https://github.com/facebookresearch/InvarianceUnitTests).
---
+++ LinearRegressionExtension/LinearRegression/train.py
@@ -0,0 +1,216 @@
+import argparse
+import hashlib
+import pprint
+import json
+import os
+import ast
+
+import datasets
+import algorithms
+import utils
+import torch
+
+# nonlinear algorithms that can be passed as arguments to extension experiments
+def relu(x, **kwargs):
+    return torch.relu(x)
+
+def sigmoid(x, **kwargs):
+    return torch.sigmoid(x)
+
+def poly(x, degree=2, **kwargs):
+    return x ** degree
+
+def sin(x, **kwargs):
+    return torch.sin(x)
+
+def additive_noise(x, noise_std=0.1, **kwargs):
+    return x + torch.randn_like(x) * noise_std
+
+def rand_ffeats(x, n_ffeats=5, **kwargs):
+    ffeats = torch.randn(x.shape[1], n_ffeats)
+    return x @ ffeats
+
+def gaussian_process(x, length_scale=1.0, variance=1.0, **kwargs):
+    # Simple RBF kernel-based Gaussian Process
+    pairwise_dists = torch.cdist(x, x) ** 2
+    K = variance * torch.exp(-0.5 * pairwise_dists / length_scale**2)
+    return K @ torch.randn(x.shape[0], 1)
+
+def monotone_transform(x, monotonicity=1.0, **kwargs):
+    return monotonicity * x + (1 - monotonicity) * torch.relu(x)
+
+def post_nonlinear(x, **kwargs):
+    # nonlinear applied to additive noise fn
+    return monotone_transform(additive_noise(x, **kwargs), **kwargs)
+
+nonlinear_algos = {
+    "relu": relu,
+    "sigmoid": sigmoid,
+    "poly": poly,
+    "sin": sin,
+    "additive_noise": additive_noise,
+    "rand_ffeats": rand_ffeats,
+    "gaussian_process": gaussian_process,
+    "monotone_transform": monotone_transform,
+    "post_nonlinear": post_nonlinear
+}
+
+TEST_QUANTILES = [0.1, 0.25, 0.5, 0.75, 0.9, 0.99, 0.999]
+
+
+def edit_alg_name_w_fixed_hparam(alg_name, hparam_name, hparam_value):
+    if alg_name == "EQRM" and hparam_name == "alpha":
+        if hparam_value > 0:
+            alpha_str = f"a={hparam_value}"
+        else:
+            alpha_str = "a~=1"
+        return f"{alg_name}_{alpha_str}"
+    else:
+        # If any other algorithm names should be edited to contain the hparam value (for sweeps), put it here.
+        return alg_name
+
+
+def fit_and_save_results(alg, envs, args, results_dirname, dataset):
+    # build file name
+    md5_fname = hashlib.md5(str(args).encode('utf-8')).hexdigest()
+    results_fname = os.path.join(results_dirname, md5_fname + ".jsonl")
+
+    # fit the dataset
+    alg.fit(
+        envs=envs,
+        n_iterations=args["n_iterations"],
+        callback=args["callback"]
+    )
+
+    # compute the train, validation and test errors
+    for split in ("train", "validation", "test"):
+        key = "error_" + split
+        for k_env, env in zip(envs[split]["keys"], envs[split]["envs"]):
+            env_error = utils.compute_error(alg, *env)
+            if "p_test" in results_dirname and ("q=" not in k_env):
+                # add environment standard deviation for training envs ('q=' is an indicator of test envs)
+                env_sdv = dataset.envs[k_env]
+                args[f"{key}_{k_env}_s={env_sdv:.1f}"] = env_error
+            else:
+                args[f"{key}_{k_env}"] = env_error
+
+    try:
+        # if linear, save weights and biases
+        if args["linear"]:
+            # save weights and biases
+            w, b = alg.network.weight, alg.network.bias
+            args["w_shape"], args["b_shape"] = list(w.shape), list(b.shape)
+            args["w"], args["b"] = [round(w_.item(), 3) for w_ in w.flatten()], [round(b_.item(), 3) for b_ in b.flatten()]
+        elif not args["linear"]:
+            # save nonlinear function name and kwargs
+            args["nonlinear_fn"] = args["nonlinear_fn"]
+            args["nonlinear_fn_kwargs"] = json.loads(args["nonlinear_fn_kwargs"])
+    except Exception as e:
+        print(f"Error saving weights/biases or nonlinear function info: {args["alg"]}, {args["linear"]}, {args["nonlinear_fn"]}, {e}")
+    # write results
+    results_file = open(results_fname, "w")
+    results_file.write(json.dumps(args))
+    results_file.close()
+
+    return args
+
+
+def run_experiment(args):
+    # build directory name
+    results_dirname = os.path.join(args["output_dir"], args["test_env_type"])
+    os.makedirs(results_dirname, exist_ok=True)
+
+    # create dataset
+    utils.set_seed(args["data_seed"])
+    dataset = datasets.DATASETS[args["dataset"]](
+        dim_inv=args["dim_inv"],
+        dim_spu=args["dim_spu"],
+        n_envs=args["n_envs"],
+        verbose=args["verbose"],
+        linear=args["linear"],
+        nonlinear_fn=nonlinear_algos[args["nonlinear_fn"]] if not args["linear"] else None,
+        nonlinear_fn_kwargs=json.loads(args["nonlinear_fn_kwargs"]) if not args["linear"] else None
+    )
+
+    # oracle (ERM trained on test mode, i.e. scrambled effect features X_2)
+    train_split = "train" if args["alg"] != "Oracle" else "test"
+
+    # sample the envs
+    envs = {}
+    for key_split, split in zip(("train", "validation", "test"), (train_split, train_split, "test")):
+        envs[key_split] = {"keys": [], "envs": []}
+        for env in dataset.envs:
+            envs[key_split]["envs"].append(dataset.sample(
+                n=args["n_samples"],
+                env=env,
+                split=split)
+            )
+            envs[key_split]["keys"].append(env)
+
+    # sample separate test envs at different quantiles of the env distribution
+    if args["test_env_type"] == "quantiles":
+        if not hasattr(dataset, "p_env"):
+            raise ValueError("Datasets must have a distribution over environments (dataset.p_env) in order to "
+                             f"evaluate performance at different quantiles. Chosen dataset {args['dataset']} does not.")
+        for q in TEST_QUANTILES:
+            inputs, outputs, sdv = dataset.sample(n=args["n_samples"], env=f"test_q={q}")
+            envs["test"]["envs"].append((inputs, outputs))
+            envs["test"]["keys"].append(f"q={q}_s={sdv:.1f}")
+
+    # offsetting alg seed to avoid overlap with data_seed
+    utils.set_seed(args["alg_seed"] + 1000)
+
+    # selecting alg
+    alg = algorithms.ALGORITHMS[args["alg"]](
+        in_features=args["dim_inv"] + args["dim_spu"],
+        out_features=1,
+        task=dataset.task,
+        hparams=args["hparams"],
+        linear=args["linear"]
+    )
+
+    # fix certain hparams to specified values, e.g.: 'alpha=100,lr=0.0001'
+    if args["hparams_fixed"] is not None:
+        kvs = [kv.split("=") for kv in args["hparams_fixed"].split(",")]
+        args["hparams_fixed"] = {kv[0]: ast.literal_eval(kv[1]) for kv in kvs}     # infer type of s
+        for k, v in args["hparams_fixed"].items():
+            alg.hparams[k] = v
+            args["alg"] = edit_alg_name_w_fixed_hparam(args["alg"], k, v)
+
+    # update this field for printing purposes
+    args["hparams"] = alg.hparams
+
+    # Fit single alg
+    args_ = fit_and_save_results(alg, envs, args, results_dirname, dataset)
+
+    return args_
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Synthetic invariances')
+    parser.add_argument('--alg', type=str, default="ERM")
+    parser.add_argument('--n_iterations', type=int, default=1000)
+    parser.add_argument('--hparams', type=str, default="default")
+    parser.add_argument('--dataset', type=str, default="Example1")
+    parser.add_argument('--dim_inv', type=int, default=1)
+    parser.add_argument('--dim_spu', type=int, default=1)
+    parser.add_argument('--n_envs', type=int, default=1000)
+    parser.add_argument('--test_env_type', type=str, default="quantiles", choices=["quantiles", "shuffled"])
+    parser.add_argument('--n_samples', type=int, default=200000)
+    parser.add_argument('--data_seed', type=int, default=0)
+    parser.add_argument('--alg_seed', type=int, default=0)
+    parser.add_argument('--output_dir', type=str, default="results/test_run")
+    parser.add_argument('--callback', action='store_true')
+    parser.add_argument('--verbose', action='store_true')
+    parser.add_argument('--hparams_fixed', type=str, help="Hparams to fix for grid sweeps. E.g.: alpha=0.9,lr=0.01")
+    parser.add_argument('--linear', type=lambda x: x.lower() == 'true', default=False)
+    parser.add_argument('--nonlinear_fn', type=str, default="relu")
+    parser.add_argument('--nonlinear_fn_kwargs', type=str, default="{}")
+    args = parser.parse_args()
+
+    # Print results to 3 decimal places
+    results = run_experiment(vars(args))
+    for k, v in results.items():
+        if isinstance(v, float):
+            results[k] = round(v, 3)
+    pprint.pprint(results)
